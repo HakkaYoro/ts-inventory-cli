86c6fa071c4ee9604b7088b39c816292ba2766aa
HakkaYoro <hackeryolo.hy@gmail.com>
2026-08-13 13:03:50 -0400

    practice: task5 Set.has vs Array.includes (WIP - params fix pending)

--- practice/0811/tasks/task5.ts
+++ practice/0811/tasks/task5.ts
@@ -25,3 +25,37 @@
 // Al terminar, EXPLICA:
 //   - ¿Por qué Set.has() es más rápido que Array.includes()?
 //   - ¿Cuándo usarías un Array y cuándo un Set?
+
+type tituloToho = string[]
+
+const gameToho: tituloToho =[
+    "EoSD",
+    "PCB",
+    "Soku",
+    "GFW",
+    "MoF"
+]
+
+//Operación O(n), no sabe que hay ahí, tiene que buscar uno a uno cada elemento. Si cada elemento se tarda 1s en revisar, 10 tomaría 10s, 1000 tomaría 1000s, 10000 tomaría 10000s, y así sucesivamente.
+function tohoJugado (gameToho: string) {
+    if (gameToho.includes("Soku")) 
+        return 'Jugado'
+    else
+        return 'Pero chiamo </3'
+} 
+
+//Operación O(1), sabe que hay ahí. Pre-indexado/analizado. Cómo sabe que hay ahí, puede tener 999999 elementos, pero siempre va a tardar lo mismo para encontrar un dato.
+const setToho = new Set(gameToho)
+
+function setTohoJugado (gameToho: string){
+    if (setToho.has("GFW")) 
+        return 'Jugado'
+    else
+        return 'Pero chiamo </3'
+}
+
+/*
+Cómo dije más arriba, .has es más rápido que .includes porque ya sabe que hay en el array de elementos.
+Array sería más que todo si tengo tablas/listas de elementos que constantemente (y de forma dinámica) cambian (aumentan, disminuyen, cambian valores).
+El Set sería si son elementos que siempre van a estar estáticos. Los hashea.
+*/
